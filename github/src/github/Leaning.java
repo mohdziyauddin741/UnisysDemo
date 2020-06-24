68c32eafefe16e7acf5e0ed58b6a8bdefbf5babe
Zee <Zee@Zeeya>
2020-06-24 11:11:03 +0530

    second commit

--- github/src/github/Leaning.java
+++ github/src/github/Leaning.java
@@ -5,7 +5,6 @@ public class Leaning {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Ziya!");
-
+		System.out.println("Checking commit and push");
 	}
-
 }
